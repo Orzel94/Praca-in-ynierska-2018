5bc0598d1fe6e60de0ff4202b9202a6b7ee00502
Unknown <mateusz.maz.94@gmail.com>
2017-11-06 23:00:17 +0100

    scalanie projektów i refactor trasy

---
+++ Urban Traffic Simulator/Assets/Scripts/PathFinding/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+    public bool isRespawnNode = false;
+    public bool isEndNode = false;
+    public Vector3 currentPosition;
+    public GameObject nextNode1;
+    public GameObject nextNode2;
+    public GameObject nextNode3;
+    private void Start()
+    {
+        currentPosition = GetComponent<Transform>().position;
+    }
+}
