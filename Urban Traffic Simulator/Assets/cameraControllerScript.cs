806a0ab4ac553b67cb415d93df85ea4a60550df7
Unknown <mateusz.maz.94@gmail.com>
2017-12-19 23:06:13 +0100

    cameras v2

---
+++ Urban Traffic Simulator/Assets/cameraControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControllerScript : MonoBehaviour {
+    public GameObject mainCamera;
+    public Camera main;
+	// Use this for initialization
+	void Start () {
+        Camera[] cameras = new Camera[200];
+        Camera.GetAllCameras(cameras);
+        foreach (var item in cameras)
+        {
+            if (item!=main)
+            {
+                item.enabled = false;
+            }
+            
+        }
+        mainCamera.GetComponent<Camera>().enabled = true;
+        main.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
