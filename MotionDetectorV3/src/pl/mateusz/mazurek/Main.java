056cf9cdd3bb08ab4b29e5f6dae37b83e84579db
Unknown <mateusz.maz.94@gmail.com>
2018-01-10 12:06:27 +0100

    refactor

--- MotionDetectorV3/src/pl/mateusz/mazurek/Main.java
+++ MotionDetectorV3/src/pl/mateusz/mazurek/Main.java
@@ -28,37 +28,36 @@ public class Main {
     }
 }*/
 
-import javafx.scene.effect.GaussianBlur;
-import org.opencv.calib3d.Calib3d;
 import org.opencv.core.*;
-import org.opencv.features2d.*;
 import org.opencv.highgui.Highgui;
 
 import java.io.File;
 import java.util.*;
 
-import org.opencv.calib3d.Calib3d;
-import org.opencv.core.*;
-import org.opencv.features2d.*;
-import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
 
-import java.io.File;
-import java.util.LinkedList;
-import java.util.List;
-
 import static org.opencv.core.Core.*;
-import static org.opencv.core.CvType.CV_32FC1;
-import static org.opencv.core.CvType.CV_8UC1;
 import static org.opencv.imgproc.Imgproc.*;
 
-/**
- *
- * Created by Kinath on 8/6/2016.
- */
-public class Main {
 
+public class Main {
+    static File folder = new File("rsc");
+    static File[] listOfFiles = folder.listFiles();
+    static ArrayList<String> fileNames=new ArrayList<>();
+    static String[] inputResultFileName=new String[1];
+
+    String frame1;
+    String frame2;
+
+    //System.out.println("Started....");
+    //System.out.println("Loading images...");
+    static Mat image1;
+    static Mat image2;
+    static Mat result;
+    static Mat result1;
+    static Mat result2;
+    static Mat result3;
     /*public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         File lib = null;
@@ -73,93 +72,103 @@ public class Main {
 */
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        result=new Mat();
+        result1=new Mat();
+        result2=new Mat();
+        result3=new Mat();
+        System.out.println("Welcome to MotionDetector V3..... Let's start!");
 
-        System.out.println("Welcom to MotionDetector V3..... Let's start!");
-
-        File folder = new File("rsc");
-        File[] listOfFiles = folder.listFiles();
-        ArrayList<String> fileNames=new ArrayList<>();
-        String inputResultFileName;
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                System.out.println("File " + listOfFiles[i].getName());
-                char[] extension=new char[3];
-                listOfFiles[i].getName().getChars(listOfFiles[i].getName().length()-3,listOfFiles[i].getName().length(),extension,0);
-                if(extension.equals("txt")){
-
-                }
-                fileNames.add(listOfFiles[i].getName());
-            } else if (listOfFiles[i].isDirectory()) {
-                System.out.println("Directory " + listOfFiles[i].getName());
-            }
-        }
-
-
-        Collections.sort(fileNames, new Comparator<String>() {
-            public int compare(String o1, String o2) {
-                return extractInt(o1) - extractInt(o2);
-            }
-
-            int extractInt(String s) {
-                String num = s.replaceAll("\\D", "");
-                // return 0 if no digits found
-                return num.isEmpty() ? 0 : Integer.parseInt(num);
-            }
-        });
+        LoadFiles();
         for (String item : fileNames ) {
             System.out.println(item);
         }
+        String frame1;
+        String frame2;
+        ArrayList<ArrayList<Double>> detections=new ArrayList<>();
+        for(int i=0;i<fileNames.size()-1;++i) {
+            frame1 = "rsc\\"+fileNames.get(i);
+            frame2 = "rsc\\"+fileNames.get(i+1);
 
-        String bookObject = "rsc\\lena\\0771.png";
-        String bookScene = "rsc\\lena\\0772.png";
+            image1 = Highgui.imread(frame1, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+            image2 = Highgui.imread(frame2, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
 
-        //System.out.println("Started....");
-        //System.out.println("Loading images...");
-        Mat objectImage = Highgui.imread(bookObject, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-        Mat sceneImage = Highgui.imread(bookScene, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-        Mat result=new Mat();
-        Mat result1=new Mat();
-        Mat result2=new Mat();
-        Mat result3=new Mat();
-        Mat gausianresult=new Mat();
-        Highgui.imwrite("rsc\\lena\\obj.jpg",objectImage);
-        absdiff(objectImage,sceneImage,result);
-        threshold(result,result1,20,255,THRESH_BINARY);
-        blur(result1,result2, new Size(20,20));
-        erode(result1,result2,getStructuringElement(MORPH_RECT,new Size(1,1)));
-        dilate(result2,result3,getStructuringElement(MORPH_RECT,new Size(40,40)));
+            PreprocesImages();
+            //Highgui.imwrite("rsc\\lena\\obj.jpg",image1);
 
 
-        Highgui.imwrite("rsc\\lena\\res.jpg",result);
-        Highgui.imwrite("rsc\\lena\\res1.jpg",result1);
-        Highgui.imwrite("rsc\\lena\\res2.jpg",result2);
-        Highgui.imwrite("rsc\\lena\\res3.jpg",result3);
+
+            /*Highgui.imwrite("rsc\\lena\\res.jpg",result);
+            Highgui.imwrite("rsc\\lena\\res1.jpg",result1);
+            Highgui.imwrite("rsc\\lena\\res2.jpg",result2);
+            Highgui.imwrite("rsc\\lena\\res3.jpg",result3);*/
 
 
-        //tracking
-        ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-        Imgproc.findContours(result3, contours, new Mat(), Imgproc.RETR_LIST,Imgproc.CHAIN_APPROX_SIMPLE);
+            //tracking
+            ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+            Imgproc.findContours(result3, contours, new Mat(), Imgproc.RETR_LIST,Imgproc.CHAIN_APPROX_SIMPLE);
+            Double x=0.0;
+            Double y=0.0;
+            if(i==0){
+                detections.add(new ArrayList<>());
+            }
+            detections.add(new ArrayList<>());
+            for (int j=0;j<contours.size();j++){
+                Moments moment = Imgproc.moments(contours.get(j));
+                double area = moment.get_m00();
+                if (area>40) {
+                    System.out.println("area: " + String.valueOf(area));
+                    x = (moment.get_m10() / area)/1360;
+                    System.out.println("x: " + String.valueOf(x));
+                    y = (moment.get_m01() / area)/768;
+                    System.out.println("y: " + String.valueOf(y));
+                }
+                if(i==0){
+                    Scanner in = new Scanner(fileNames.get(i)).useDelimiter("[^0-9]+");
+                    int integer = in.nextInt();
+                    detections.get(i).add((double) integer);
+                    detections.get(i).add(x);
+                    detections.get(i).add(y);
 
-        for (int i=0;i<contours.size();i++){
-            Moments moment = Imgproc.moments(contours.get(i));
-            double area = moment.get_m00();
-            if (area>40) {
-                System.out.println("area: " + String.valueOf(area));
-                double x = (moment.get_m10() / area)/1360;
-                System.out.println("x: " + String.valueOf(x));
-                double y = (moment.get_m01() / area)/768;
-                System.out.println("y: " + String.valueOf(y));
+
+                    in = new Scanner(fileNames.get(i+1)).useDelimiter("[^0-9]+");
+                    integer = in.nextInt();
+                    detections.get(i+1).add((double) integer);
+                    detections.get(i+1).add(x);
+                    detections.get(i+1).add(y);
+                }else{
+                    Scanner in = new Scanner(fileNames.get(i+1)).useDelimiter("[^0-9]+");
+                    int integer = in.nextInt();
+                    detections.get(i+1).add((double) integer);
+                    detections.get(i+1).add(x);
+                    detections.get(i+1).add(y);
+                }
+            }
+
+        }
+        File file = new File("rsc\\"+inputResultFileName[0]);
+        try {
+            Scanner scanner = new Scanner(file);
+            scanner.useLocale(Locale.US);
+            while (scanner.hasNext()) {
+                int x = scanner.nextInt();
+                double y = scanner.nextDouble();
+                double z = scanner.nextDouble();
+                System.out.println("x="+x+", y="+y+", z="+z);
             }
         }
+        catch(Exception e){
+
+        }
+
 
 ///////////////////////////////////////////////////////////
       /*  Rect rect = Imgproc.boundingRect(contours.get(0));
         //Mat mask = new Mat(rect.height,rect.width,CV_8UC1);
         //erode(mask,mask,getStructuringElement(MORPH_RECT,new Size(10,10)));
-        Mat mask = new Mat(objectImage,rect);
+        Mat mask = new Mat(image1,rect);
         Highgui.imwrite("rsc\\lena\\mask.jpg",mask);
         Mat tempScene = new Mat();
-        sceneImage.copyTo(tempScene);
+        image2.copyTo(tempScene);
         int resultCols = tempScene.cols()-mask.cols()+1;
         int resultRows = tempScene.rows()-mask.rows()+1;
         Mat resTemp=new Mat();
@@ -180,23 +189,45 @@ public class Main {
         System.out.println("y: "+String.valueOf(y1));
         System.out.println("x: "+String.valueOf(matchLoc.y));*/
 
-        /*File lib = null;
 
-        String os = System.getProperty("os.name");
-        String bitness = System.getProperty("sun.arch.data.model");
-        String bookObject = "rsc\\w.jpg";
-        String bookScene = "rsc\\wo.jpg";
+    }
 
-        //System.out.println("Started....");
-        //System.out.println("Loading images...");
-        Mat objectImage = Highgui.imread(bookObject, Highgui.CV_LOAD_IMAGE_COLOR);
-        Mat sceneImage = Highgui.imread(bookScene, Highgui.CV_LOAD_IMAGE_COLOR);
-        Mat result=new Mat();
-        Mat gausianresult=new Mat();
+    private static void PreprocesImages() {
+        absdiff(image1, image2,result);
+        threshold(result,result1,20,255,THRESH_BINARY);
+        //blur(result1,result2, new Size(20,20));
+        erode(result1,result2,getStructuringElement(MORPH_RECT,new Size(1,1)));
+        dilate(result2,result3,getStructuringElement(MORPH_RECT,new Size(40,40)));
+    }
 
-        absdiff(objectImage,sceneImage,result);
+    static void LoadFiles(){
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+                char[] extension=new char[3];
+                listOfFiles[i].getName().getChars(listOfFiles[i].getName().length()-3,listOfFiles[i].getName().length(),extension,0);
+                if(String.valueOf(extension).equals("txt")){
+                    inputResultFileName[0]=listOfFiles[i].getName();
+                }else{
+                    fileNames.add(listOfFiles[i].getName());
+                }
 
-        Highgui.imwrite("rsc\\res.jpg",result);
-        System.out.println("Ended....");*/
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+
+
+        Collections.sort(fileNames, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                return extractInt(o1) - extractInt(o2);
+            }
+
+            int extractInt(String s) {
+                String num = s.replaceAll("\\D", "");
+                // return 0 if no digits found
+                return num.isEmpty() ? 0 : Integer.parseInt(num);
+            }
+        });
     }
 }
